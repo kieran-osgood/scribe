5f03c7d43ada35bfe20c808d522c15fbd7f1b3fc
Kieran Osgood <kieranbosgood@gmail.com>
2023-03-09 17:27:59 +0000

    feat: setup config module

--- src/test/template.ts
+++ src/test/template.ts
@@ -1,70 +1,38 @@
-// type FileNameFormatter = 'PascalCase' | 'camelCase' | 'lower-kebab-case' | 'Upper-Kebab-Case'
-
-type Template = {
-  template: string
-  output: {
-    directory?: string
-    fileName: string
-  }
-}
-
-type TemplateSettings = {
-  output?: {
-    directory?: string
-  }
-  outputs: Template[]
-}
-
-type Config = {
-  // Global settings that apply to all template options
-  // Overrideable within option settings
-  global: {
-    // fileNameFormatter: FileNameFormatter
-    /**
-     * Sets the root for pathing on relative paths
-     */
-    rootOutDir: string
-
-    templatesDirectories: string[]
-  },
-  // List of templates for the CLI to render
-  templateOptions: Record<string, TemplateSettings>
-}
+import { Config } from "../config/index.js";
 
 const config: Config = {
   global: {
-    rootOutDir: '.',
-    templatesDirectories: ['./templates']
+    rootOutDir: ".",
+    templatesDirectories: ["./templates"],
   },
   templateOptions: {
     screen: {
-      output: { directory: './src/screens' },
+      output: { directory: "./src/screens" },
       outputs: [
         {
-          template: 'view.ejs',
-          output: { fileName: '$NAME$View' }
+          template: "view.ejs",
+          output: { fileName: "$NAME$View" },
         },
         {
-          template: 'view-model.ejs',
-          output: { fileName: 'use$NAME$ViewModel' }
+          template: "view-model.ejs",
+          output: { fileName: "use$NAME$ViewModel" },
         },
         {
-          template: 'styles.ejs',
-          output: { fileName: '$NAME$.styles.ts', directory: './styles' }
-        }
-      ]
+          template: "styles.ejs",
+          output: { fileName: "$NAME$.styles.ts", directory: "./styles" },
+        },
+      ],
     },
     hook: {
-      output: { directory: './src/hooks' },
+      output: { directory: "./src/hooks" },
       outputs: [
         {
-          template: 'hooks.ejs',
-          output: { fileName: 'use$NAME$' }
-        }
-      ]
-    }
-
-  }
+          template: "hooks.ejs",
+          output: { fileName: "use$NAME$" },
+        },
+      ],
+    },
+  },
 };
 
 export default config;
