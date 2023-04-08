983bdbceda843276a7c03688364153e11df87be5
Kieran Osgood <kieranbosgood@gmail.com>
2023-04-08 19:18:32 +0100

    refactor: Add additional exports to fp-ts module

--- src/common/fp/index.ts
+++ src/common/fp/index.ts
@@ -1,17 +1,19 @@
-export * as IO from "fp-ts/lib/IO.js";
-export * as TE from "fp-ts/lib/TaskEither.js";
-export * as C from "fp-ts/lib/Console.js";
+// effect
+export * from 'effect';
+export * from '@effect/core';
+export * as B from '@effect/data/Boolean';
+export * as E from '@effect/data/Either';
+export * as O from '@effect/data/Option';
+//
+export * as Effect from '@effect/io/Effect';
+export * as R from '@effect/io/Runtime';
+//
+export * as S from '@effect/schema/Schema';
 
-export * as SStd from "fp-ts-std/Struct";
+// fp-ts
+export * as IO from 'fp-ts/lib/IO.js';
+export * as TE from 'fp-ts/lib/TaskEither.js';
+export * as C from 'fp-ts/lib/Console.js';
 
-export * from "effect";
-
-export * from "@effect/core";
-
-// export * from "@effect/data/Function";
-export * as E from "@effect/data/Either";
-export * as O from "@effect/data/Option";
-export * as B from "@effect/data/Boolean";
-
-// export * from "@effect/io/Effect";
-export * as S from "@effect/schema/Schema";
+// fp-ts-std
+export * as SStd from 'fp-ts-std/Struct';
