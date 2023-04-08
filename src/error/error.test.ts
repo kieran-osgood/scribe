b4556d4aefce6c397364a709f4016cf132a5251c
Kieran Osgood <kieranbosgood@gmail.com>
2023-04-08 19:16:38 +0100

    feat: move to vitest - setup fast-check

--- src/error/error.test.ts
+++ src/error/error.test.ts
@@ -1,50 +1,40 @@
-import { formatErrorMessage } from "./error";
-import { pipe } from "../common/fp";
-import { ZodError } from "zod";
-import { describe } from "vitest";
+import { formatErrorMessage } from './error';
+import { pipe } from '../common/fp';
+import { ZodError } from 'zod';
+import { describe } from 'vitest';
+import * as fc from 'fast-check';
+import * as S from 'fp-ts/lib/string';
 
-const runTest = (value: unknown, expected: string) =>
-  pipe(value, formatErrorMessage, (e) => expect(e).toBe(expected));
-
-describe("formatErrorMessage", () => {
-  it("should return the value if it was a string", () => {
-    runTest("test error", "test error");
+describe('formatErrorMessage', () => {
+  it('should format the errors if it was a Error instance', () => {
+    pipe(new Error('An error instance!'), formatErrorMessage, e =>
+      expect(e).toBe('An error instance!')
+    );
   });
 
-  it("should format the errors if it was a Error instance", () => {
-    runTest(new Error("An error instance!"), "An error instance!");
-  });
-
-  it("should return the error.message if it was a ZodError instance", () => {
+  it('should return the error.message if it was a ZodError instance', () => {
     pipe(
       new ZodError([
         {
-          code: "invalid_type",
-          expected: "string",
-          received: "number",
-          path: ["test"],
-          message: "Expected a string but you sent a number?",
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'number',
+          path: ['test'],
+          message: 'Expected a string but you sent a number?',
         },
-      ]), //
+      ]),
       formatErrorMessage,
-      (e) =>
+      e =>
         expect(e).toMatchInlineSnapshot(
           '"{\\"_errors\\":[],\\"test\\":{\\"_errors\\":[\\"Expected a string but you sent a number?\\"]}}"'
         )
     );
   });
-  describe("should not stringify when given:", () => {
-    it("null", () => {
-      runTest(null, "null");
-    });
-    it("undefined", () => {
-      runTest(undefined, "undefined");
-    });
-    it("number", () => {
-      runTest(0, "0");
-    });
-    it("array", () => {
-      runTest(["abc", "123"], "abc,123");
-    });
-  });
+
+  it('non Error values should stringify', () =>
+    fc.assert(
+      fc.property(fc.anything({ withObjectString: true }), _ =>
+        S.Eq.equals(formatErrorMessage(_), String(_))
+      )
+    ));
 });
