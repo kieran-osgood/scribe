a0c74f605ee9400074ba5a236cea7fd5113d4a0a
Kieran Osgood <kieranbosgood@gmail.com>
2023-04-28 23:34:00 +0100

    refactor: rename index to main
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- index.ts
+++
@@ -1,7 +0,0 @@
-#!/bin/env node
-
-import { run } from "./src/program.js";
-
-(async () => {
-  await run();
-})();
---
+++ main.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { run } from './src/program.js';
+
+(async () => {
+  await run();
+})();
