93cd6303d13f5ee7221aa578447d8e441cd2ab24
Kieran Osgood <kieranbosgood@gmail.com>
2023-04-28 23:42:25 +0100

    feat: setup generator to load all deps
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- src/program.ts
+++ src/program.ts
@@ -2,7 +2,7 @@ import { readConfigFlag, readFlags } from './reader/arguments.js';
 import * as Effect from '@effect/io/Effect';
 import { readConfig, readUserTemplateOptions } from './reader/config';
 import { readPrompt } from './reader/prompt';
-import { Exit } from './common/fp';
+import { Exit, pipe } from './common/fp';
 
 const ExitStatus = {
   success: 0,
@@ -10,15 +10,42 @@ const ExitStatus = {
 } as const;
 
 export async function run() {
-  return Effect.runPromiseExit(program).then(
-    Exit.mapBoth(
-      _ => {
-        console.log('Run error: ', _);
-        process.exit(ExitStatus.error);
-      },
-      _ => console.log('result: ', _)
+  return Effect.runPromiseExit(
+    pipe(
+      program,
+      Effect.map(_ => {
+        console.log('abc: ', _);
+        return _;
+      }),
+      Effect.map(_ => {
+        _.config.templateOptions;
+        _.input.template;
+        const createFilePath = Effect.succeed('');
+
+        // pipe(
+        //   createFilePath, //
+        //
+        //   Effect.flatMap(_ => FS.fileExists(_)),
+        //   //
+        //   Effect.provideSomeLayer(FS.LiveFS)
+        // );
+
+        /**
+         * 1. create template file path
+         * 1. check the template file exists
+         * 1.1 if so, parse it for syntax
+         * 1.2 if not, die
+         * 2. create the filename
+         * 3. create the file
+         * 4. write the file
+         * 5. exit
+         */
+      }),
+      Effect.map(_ => {
+        return 'Complete!';
+      })
     )
-  );
+  ).then(logAndExit);
 
   // // Write files based on selections.template option
   // const fileName = fileNameFormatter(
@@ -34,5 +61,17 @@ const program = Effect.gen(function* ($) {
   const flags = yield* $(readFlags(templates));
   const input = yield* $(readPrompt({ templates, flags }));
 
-  return { configPath, config, templates, input, flags };
+  return {
+    config,
+    input,
+    // flags
+  };
 });
+
+const logAndExit = Exit.mapBoth(
+  _ => {
+    console.log('Run error: ', _);
+    process.exit(ExitStatus.error);
+  },
+  _ => console.log('✅: ', _)
+);
