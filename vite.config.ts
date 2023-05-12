c58210f17c42466ed8404fc6d909763ba600490c
Kieran Osgood <kieranbosgood@gmail.com>
2023-05-12 22:21:49 +0100

    fix: tests failing due to absolute imports
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
+  },
+  resolve: {
+    alias: {
+      '@scribe/core': path.resolve(__dirname, 'src/common/core/index'),
+      '@scribe/config': path.resolve(__dirname, 'src/config/index'),
+      '@scribe/reader': path.resolve(__dirname, 'src/reader/index'),
+      '@scribe/git': path.resolve(__dirname, 'src/git/index'),
+      '@scribe/commands': path.resolve(__dirname, 'src/commands/index'),
+      '@scribe/fs': path.resolve(__dirname, 'src/common/fs/index'),
+    },
   },
 });
