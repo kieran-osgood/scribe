1a7c8dfa90d37e3cdc4fb648464fa03018b0a81f
Kieran Osgood <kieranbosgood@gmail.com>
2023-05-12 21:44:21 +0100

    refactor: move git tag catching inside checkWorkTreeClean
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- src/git/git.ts
+++ src/git/git.ts
@@ -1,6 +1,7 @@
 import simpleGit, { GitError, StatusResult, TaskOptions } from 'simple-git';
 import * as Effect from '@effect/io/Effect';
 import { TaggedClass } from '@effect/data/Data';
+import { pipe } from '@scribe/core';
 
 class GitStatusError extends TaggedClass('GitStatusError')<{
   readonly cause?: unknown;
@@ -20,26 +21,39 @@ class GitStatusError extends TaggedClass('GitStatusError')<{
 }
 
 export const checkWorkingTreeClean = (options?: TaskOptions) =>
-  Effect.asyncInterrupt<never, GitStatusError, StatusResult>(resume => {
-    const controller = new AbortController();
-    const git = simpleGit({ abort: controller.signal });
+  pipe(
+    Effect.asyncInterrupt<never, GitStatusError, StatusResult>(resume => {
+      const controller = new AbortController();
+      const git = simpleGit({ abort: controller.signal });
 
-    git.status(options, (cause, status) => {
-      // TODO: remove development flags
-      if (process.env.NODE_ENV === 'development') {
-        resume(Effect.succeed(status));
-      }
+      git.status(options, (cause, status) => {
+        // TODO: remove development flags
+        if (process.env.NODE_ENV === 'development') {
+          resume(Effect.succeed(status));
+        }
 
-      if (cause) {
-        resume(Effect.fail(new GitStatusError({ status, cause })));
-      }
+        if (cause) {
+          resume(Effect.fail(new GitStatusError({ status, cause })));
+        }
 
-      if (status.isClean()) {
-        resume(Effect.succeed(status));
+        if (status.isClean()) {
+          resume(Effect.succeed(status));
+        } else {
+          resume(Effect.fail(new GitStatusError({ status })));
+        }
+      });
+
+      return Effect.succeed(() => controller.abort());
+    }),
+
+    Effect.catchTag('GitStatusError', _ => {
+      if (_.status.isClean() === false) {
+        // Not clean - Kick off Effect prompt for continue dangerously
+        console.log(_.toString());
       } else {
-        resume(Effect.fail(new GitStatusError({ status })));
+        // Unknown error/not git - Kick off Effect prompt for continue dangerously
+        console.log(_.toString());
       }
-    });
-
-    return Effect.succeed(() => controller.abort());
-  });
+      return Effect.succeed('');
+    })
+  );
