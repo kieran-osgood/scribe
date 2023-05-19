74551abd2a3f8f1b8633363406a7fd1f3bc413c0
Kieran Osgood <kieranbosgood@gmail.com>
2023-05-20 00:52:54 +0100

    refactor(examples): setup examples as input dir
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- examples/scribe.config.ts
+++ examples/scribe.config.ts
@@ -3,7 +3,7 @@ import type { ScribeConfig } from '@scribe/config';
 const config = {
   options: {
     rootOutDir: '.',
-    templatesDirectories: ['.'],
+    templatesDirectories: ['./examples'],
   },
   templates: {
     screen: {
