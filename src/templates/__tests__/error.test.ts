bc823af826e766905643407fbaf0de83963ed84c
Kieran Osgood <kieranbosgood@gmail.com>
2023-05-28 22:21:48 +0100

    chore(templates/error): add error test
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

---
+++ src/templates/__tests__/error.test.ts
@@ -0,0 +1,10 @@
+import { TemplateFileError } from '../error';
+
+describe('error', () => {
+  describe(TemplateFileError.name, () => {
+    it('toString', () => {
+      const result = new TemplateFileError({ cause: '' }).toString();
+      expect(result).toMatchInlineSnapshot('"Writing to file failed, please report this."');
+    });
+  });
+});
