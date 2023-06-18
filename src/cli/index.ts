631a99dfcb056ecdd1d37dddf177ae0525aa788e
Kieran Osgood <kieranbosgood@gmail.com>
2023-06-18 21:39:24 +0100

    fix: help/version commands broken
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,8 +1,8 @@
 import { Effect } from '@scribe/core';
 import { Builtins, Cli } from 'clipanion';
-import { DefaultCommand } from './commands/DefaultCommand';
 import { CliError } from './error';
 import { BaseContext } from 'clipanion/lib/advanced/Cli';
+import { DefaultCommand } from './commands/DefaultCommand';
 
 export function _Cli(args: string[], contextOverrides?: Partial<BaseContext>) {
   const cli = new Cli({
@@ -10,16 +10,17 @@ export function _Cli(args: string[], contextOverrides?: Partial<BaseContext>) {
     binaryName: 'scribe',
     enableCapture: true,
   });
+
   cli.register(DefaultCommand);
   // cli.register(InitCommand);
+
   cli.register(Builtins.HelpCommand);
   cli.register(Builtins.VersionCommand);
-
   return cli.runExit(args, { ...Cli.defaultContext, ...contextOverrides });
 }
 
 export function run(
-  args: string[] = process.argv.slice(2),
+  args: string[] = process.argv,
   contextOverrides: Partial<BaseContext> = {}
 ) {
   return Effect.tryCatchPromise(
