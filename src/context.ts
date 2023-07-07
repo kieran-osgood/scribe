6617900002f43c01e9bee72c9c5abbeb9803b010
Kieran Osgood <kieranbosgood@gmail.com>
2023-07-07 22:34:35 +0100

    refactor(context): extract config path construction and use for all readings
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- src/context.ts
+++ src/context.ts
@@ -1,39 +1,35 @@
 import { Boolean, Effect, pipe } from 'src/core';
 import * as Config from '@scribe/config';
 import { Process } from '@scribe/services';
-
 import * as Prompt from 'src/services/prompt';
 import path from 'path';
 
-interface ProgramInputs {
+interface Flags {
   name: string | undefined;
   template: string | undefined;
   configPath: string;
 }
-export const promptUserForMissingArgs = (inputs: ProgramInputs) =>
+
+export const promptUserForMissingArgs = (flags: Flags) =>
   Effect.gen(function* ($) {
-    const _process = yield* $(Process.Process);
-    const config = yield* $(
-      pipe(
-        path.isAbsolute(inputs.configPath),
-        Boolean.match(
-          () => path.join(_process.cwd(), inputs.configPath),
-          () => inputs.configPath,
-        ),
-        Config.readConfig,
-      ),
-    );
+    const configPath = yield* $(constructConfigPath(flags.configPath));
+    const config = yield* $(Config.readConfig(configPath));
+    const templates = yield* $(Config.readUserTemplateOptions(configPath));
+    const input = yield* $(Prompt.launchPromptInterface({ templates, flags }));
 
-    const templateKeys = yield* $(
-      Config.readUserTemplateOptions(inputs.configPath),
-    );
-
-    const input = yield* $(
-      Prompt.launchPromptInterface({
-        templates: templateKeys,
-        flags: { template: inputs.template, name: inputs.name },
-      }),
-    );
-
-    return { config, input, templateKeys } as const;
+    return { config, input, templateKeys: templates } as const;
   });
+
+const constructConfigPath = (filePath: string) =>
+  pipe(
+    Process.Process,
+    Effect.map(_process =>
+      pipe(
+        path.isAbsolute(filePath),
+        Boolean.match(
+          () => path.join(_process.cwd(), filePath),
+          () => filePath,
+        ),
+      ),
+    ),
+  );
