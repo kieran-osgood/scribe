814aabbc73ec60d5e0f8b5b55a5bc44f434c9f8a
Kieran Osgood <kieranbosgood@gmail.com>
2023-07-23 19:20:36 +0100

    fix: remove esm build output
    Signed-off-by: Kieran Osgood <kieranbosgood@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   // watch: true,
   treeshake: 'safest',
-  format: ['cjs', 'esm'],
+  format: ['cjs'],
   clean: true,
   silent: true,
   env: {
