69349e3d5cd3cb467caf3eadb219e00ca678b2a9
Kieran Osgood <kieranbosgood@gmail.com>
2024-05-10 12:05:34 +0100

    fix: pipeline issue missing scribe config file

---
+++ scribe.config.ts
@@ -0,0 +1,9 @@
+import { ScribeConfig } from './packages/cli/index';
+
+export default {
+  options: {
+    rootOutDir: '.',
+    templatesDirectories: ['.'],
+  },
+  templates: {},
+} satisfies ScribeConfig;
