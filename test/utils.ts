8fb6c5ecacf87cecdc0a42178234168ee63b2ead
Kieran Osgood <kieranbosgood@gmail.com>
2024-05-13 15:44:52 +0100

    fix: move default branch to init command

--- test/utils.ts
+++ test/utils.ts
@@ -95,11 +95,7 @@ export function createMinimalProject(_options?: CreateMinimalProjectOptions) {
       cwd: tmpPath,
     } satisfies child_process.ExecSyncOptionsWithBufferEncoding;
 
-    child_process.execSync(
-      'git config --global init.defaultBranch main',
-      execOpts,
-    );
-    child_process.execSync('git init', execOpts);
+    child_process.execSync('git init --initial-branch main', execOpts);
     child_process.execSync('git config user.name "kieran"', execOpts);
     child_process.execSync('git config user.email "ko@gmail.com"', execOpts);
     if (!options.git.dirty) {
